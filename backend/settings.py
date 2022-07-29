c15ae9de1d1067368f4dadc61f837385e6860132
tkomenoi <marese.tkomenoi@gmail.com>
2022-07-30 02:24:39 +0900

    init!

--- backend/settings.py
+++ backend/settings.py
@@ -1,11 +1,15 @@
 import os
 from datetime import timedelta
+import environ
+
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0g)nj5-c3zke(vu3u7n&_j7s+cfl&=a5cmailajg(ru+qh29%q'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
